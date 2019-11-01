06ed1abbc2a6e4bc8a1216efc57756ef2521084d
davidhs711 <dhorts00@estudiantes.unileon.es>
2019-11-01 18:04:34 +0100

    Funciona la clase Date y el metodo nombreMes

--- p-date/src/es/unileon/prg/date/Date.java
+++ p-date/src/es/unileon/prg/date/Date.java
@@ -1,113 +1,120 @@
 package es.unileon.prg.date;
 
 public class Date {
-	private int i;
+
 	private int day;
 	private int month;
 	private int year;
-	
-	public Date(int day, int mounth, int year) throws DateException{
-		/*
-		- Este thows DayException tiene que quedarse porque aunque dentro no haya un 
-		- throws los metodos llamados si que lo tienen y se "propaga"
-		*/
-		StringBuilder error= new StringBuilder();
-		
-		try{
-			this.setMonth(month);
-			this.setDay(day);
-			this.satYear(year);
-		} catch(DateException e){
-			error.append(e.getMessage());
-		}
+	String mes;
+
+	public Date (int day, int month, int year) throws DateException {
+			StringBuilder error = new StringBuilder();
+			try {
+				this.setMonth(month);
+				this.setDay(day);
+			} catch (DateException e) {
+				error.append(e.getMessage());
+			}
+
+			this.setYear(year);
 	}
-
-	/*
-	public void setDay(int day) throws DateException{
-		
-		StringBuilder error= new StringBuilder();
-
-		if((month<1) || (month>12)){
-			//Error mes
-			error.append("- Date error: month value "+".../"+month+"/..."+" not valid. Should be 1 to 12");
-
-		} else if((day<1) || (day>this.daysOfMonth(month))){
-			//Error dia
-
-		}
-		
-		if(year<0){
-			//Error año
-		
-		}
-		
-		if(error.length()>0){
-			throw new DateException(error.toString());
-		
-		} else{
-			this.day= day;
-			this.month= month;
-			this.year= year;		
-		}
-	}
-	*/
 
 	public void setMonth(int month) throws DateException{
-		if((month<1) || (month>12)){
-			//Error mes
-			throw new DateException("- Date error: month value "+".../"+month+"/..."+" not valid. Should be 1 to 12");
-		} else{
+		if (month<1 || month>12) {
+			throw new DateException("Date error: month value "+month+" not valid. Should be from 1 to 12.");
+		} else {
 			this.month= month;
 		}
 	}
-
+	
 	public void setDay(int day) throws DateException{
-		if((day<1) || (day>this.daysOfMonth(month))){
-			//Error dia
-			throw new DateException("noseq");
-
-		} else{
-			this.day= day;
+		if (day<1 || day>this.daysOfMonth(this.month)) {
+			throw new DateException("Date error: day value "+day+" not valid. Value should be from 1 to "+daysOfMonth(this.month));
+		} else {
+			this.day=day;
 		}
-	}
-
-
-	private int daysOfMonth(int i){
-		int diaMax= 0;
-		switch (i){
-			case 1: //next 
-			case 2: diaMax=28; break;
-			case 3: //next
-			case 4: //next
-			case 5: //next
-			case 6: //next
-			case 7: //next
-			case 8: //next
-			case 9: //next
-			case 10: //next
-			case 11: diaMax=30; break;
-			case 12: diaMax=31; break;
-		}
-		return diaMax;
 	}
 	
+	public void setYear(int year) throws DateException{
+		if(year<0) {
+			throw new DateException("Date error: year value "+year+" not valid. Value should be bigger than 0");
+		} else {
+			this.year= year;
+		}
+	}
+	
+	public String nombreMes() {
+		
+		switch(month){
+			case 1: mes= "Enero"; break;
+			case 2: mes= "Febrero"; break;
+			case 3: mes= "Marzo"; break;
+			case 4: mes= "Abril"; break;
+			case 5: mes= "Mayo"; break;
+			case 6: mes= "Junio"; break;
+			case 7: mes= "Julio"; break;
+			case 8: mes= "Agosto"; break;
+			case 9: mes= "Septiembre"; break;
+			case 10: mes= "Octubre"; break;
+			case 11: mes= "Noviembre"; break;
+			case 12: mes= "Diciembre"; break;
+		}
+		return mes;
+	}
+	
+	
+	
+
+	// public void setMonth(int month) throws Date ACABAR
+		/* StringBuilder error = new StringBuilder();
+		if (month<1 || month>12) {
+			error.append("Date error: month value"+month+"not valid. Value should be from 1 to 12."); //Error mes
+		} else if (day<1 || day>this.daysOfMonth(month)) {
+		//Error dia
+		}
+		
+		if (year<0) {
+			//Error year
+	}
+	if (error.length()>0) {
+		throw new DateException(error.toString());
+	} else {
+		this.day=day;
+		this.month=month;
+		this.year=year;
+}
+*/
+
+	private int daysOfMonth(int i){
+		int diaMax=0;
+		switch (i){
+		case 1: //next
+		case 3: //next
+		case 5: //next
+		case 7: //next
+		case 8: //next
+		case 10: //next
+		case 12: diaMax=31; break;
+		case 4: //next
+		case 6: //next
+		case 9: //next
+		case 11: diaMax=30; break;
+		case 2: diaMax=28; break;
+		}
+		
+		return diaMax;
+	}
+
 	public int daysOfMonth(){
 		return this.daysOfMonth(this.month);
 	}
 
 	public int countDaysFromYearStart(){
 		int count=this.day;
-		int prueba= 3;
-		for(i=1; i<this.month; i++){
-			count+= this.daysOfMonth(i); //Puede usar daysOfMonth potrque es un metodo hermano en esta clase
+		for (int i=1; i<this.month; i++){
+			count += this.daysOfMonth(i); // Puedo usar daysOfMonth sin nada delante por se un métdo hermano de la misma clase
 		}
+		
 		return count;
 	}
-
-
-
-
 }
-
-
-
--- p-date/src/es/unileon/prg/date/MainDate.java
+++ p-date/src/es/unileon/prg/date/MainDate.java
@@ -1,16 +1,22 @@
 package es.unileon.prg.date;
 
 public class MainDate {
+
 	public static void main(String[] args) {
 
 		Date today;
 
 		try {
-			today = new Date(99, 14, 2017);	
+			today = new Date(01, 6, 2019);
 			System.out.println(today.toString());
+			System.out.println("El mes tiene estos dias: " +today.daysOfMonth());
+			System.out.println("El nombre del mes es: " +today.nombreMes());
+			//System.out.println("La estacion del año es: " +today.estacionAño());
+			//System.out.println("Los meses que quedan del año son: " +today.mesesQuedanAño());
 		} catch (DateException e) {
 			System.out.println(e.getMessage());
 		}
-
+		
 	}
+
 }
